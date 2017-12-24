1d2c1e75ee7db17f96043d7cf77349aa13cb30ae
Daniel <messyjones@gmail.com>
2017-12-23 19:38:31 -0500

    got google to authenticate our api

---
+++ hqTrivia.js
@@ -0,0 +1,20 @@
+process.env.GOOGLE_APPLICATION_CREDENTIALS = "./trivia-testing-7bb31e4ccfb0.json";
+
+// Imports the Google Cloud client library
+const vision = require('@google-cloud/vision');
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+// Performs label detection on the image file
+client
+  .labelDetection('./resources/hq-photo.png')
+  .then(results => {
+    const labels = results[0].labelAnnotations;
+
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
