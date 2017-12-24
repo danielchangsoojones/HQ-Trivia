2c53997cbc1b97813c42797d522dda8eef030822
Daniel <messyjones@gmail.com>
2017-12-23 23:09:10 -0500

    google search api is returning the results in correct snipppets

--- search/GoogleSearchCount.js
+++ search/GoogleSearchCount.js
@@ -7,18 +7,40 @@
 var request = require('request');
 
 findGoogleSearchCount("hi");
-function findGoogleSearchCount(queryText) {
+function findGoogleSearchCount(question, answers) {
     request.get({
         url: "https://www.googleapis.com/customsearch/v1",
         qs: {
             key : 'AIzaSyCfBdbnT24Gy-_yBupr9nc4s7lklzwAwAw',
             cx : '000678387829034089016:pfmft8eezi0',
-            q : queryText
+            q : question
             }
         },
         function (error, response, body) {
-            console.log(body);
+            setScores(answers, body);
         }
     );
+}
+
+//Purpose: give each answer a score for the number of times it was mentioned in the search results
+function setScores(answers, body) {
+    var snippets = extractSearchSnippets(body);
+    console.log(snippets);
+}
+
+function findAnswerInSearchResults(searchResults) {
+
+}
+
+function extractSearchSnippets(body) {
+    var json = JSON.parse(body);
+    var searchResults = json.items;
+    var snippets = [];
+    for (var i = 0; i < searchResults.length; i++) {
+        var result = searchResults[i];
+        snippets.push(result.snippet);
+    }
+
+    return snippets;
 }
 
