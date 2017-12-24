56f94e2cf058fd8e4a521b4d6c5a0ad7f1719858
Daniel <messyjones@gmail.com>
2017-12-23 20:38:21 -0500

    got the text to return to our client. Still need to parse it

--- hqTrivia.js
+++ hqTrivia.js
@@ -6,14 +6,27 @@ const vision = require('@google-cloud/vision');
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 
-// Performs label detection on the image file
+// Performs text detection on the image file
 client
-  .labelDetection('./resources/hq-photo.png')
+  .textDetection('./resources/hq-photo.png')
   .then(results => {
-    const labels = results[0].labelAnnotations;
+    //a block is each block of text
+    var blocks = results[0].fullTextAnnotation.pages[0].blocks[5];
 
-    console.log('Labels:');
-    labels.forEach(label => console.log(label.description));
+    console.log(blocks);
+    var paragraph = blocks.paragraphs[0];
+    var words = paragraph.words;
+    console.log(words[0].symbols[0]);
+
+    // var bensThing = results[0].textAnnotations[0].description;
+
+    // console.log("Ben's thing: ");
+    // console.log(bensThing);
+
+    var question;
+    var answer1;
+    var answer2;
+    var answer3;
   })
   .catch(err => {
     console.error('ERROR:', err);
