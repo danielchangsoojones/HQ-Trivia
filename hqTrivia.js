3076d7ffcbdcb8eef9ee1c6ae9d9087d2f361343
Daniel <messyjones@gmail.com>
2017-12-24 00:57:22 -0500

    got rid of non-necessary lines

--- hqTrivia.js
+++ hqTrivia.js
@@ -10,21 +10,6 @@ const client = new vision.ImageAnnotatorClient();
 client
   .textDetection('./resources/hq-photo.png') //photo is replaced by automator with the newest screenshots
   .then(results => {
-    //a block is each block of text
-    // var blocks = results[0].fullTextAnnotation.pages[0].blocks[5];
-
-    // // console.log(blocks);
-    // var paragraph = blocks.paragraphs[0];
-    // var words = paragraph.words;
-    // console.log(words[0].symbols[0]);
-
-    console.log("heyyy");
-
-    // var bensThing = results[0].textAnnotations[0].description;
-
-    // console.log("Ben's thing: ");
-    // console.log(bensThing);
-
     var textParser = require("./visionTextJSONParser.js");
     var QuestionAnswers = textParser.parse(results);
   })
