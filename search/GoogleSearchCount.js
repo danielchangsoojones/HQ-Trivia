915bfda7d37e1c7e9341cceefdb89811ca3dbd27
Daniel <messyjones@gmail.com>
2017-12-23 23:38:52 -0500

    the google search function is working with test data of albert einstein

--- search/GoogleSearchCount.js
+++ search/GoogleSearchCount.js
@@ -6,18 +6,24 @@
 
 var request = require('request');
 
-findGoogleSearchCount("hi");
-function findGoogleSearchCount(question, answers) {
+findGoogleSearchCount({
+    question: "when was albert einstein born?",
+    answer0: "2005",
+    answer1: "2015",
+    answer2: "1879"
+});
+
+function findGoogleSearchCount(hqData) {
     request.get({
         url: "https://www.googleapis.com/customsearch/v1",
         qs: {
             key : 'AIzaSyCfBdbnT24Gy-_yBupr9nc4s7lklzwAwAw',
             cx : '000678387829034089016:pfmft8eezi0',
-            q : question
+            q : hqData.question
             }
         },
         function (error, response, body) {
-            setScores(answers, body);
+            setScores(hqData, body);
         }
     );
 }
@@ -25,20 +31,38 @@ function findGoogleSearchCount(question, answers) {
 //Purpose: give each answer a score for the number of times it was mentioned in the search results
 function setScores(answers, body) {
     var snippets = extractSearchSnippets(body);
-    console.log(snippets);
+    var scores = findAnswerInSearchResults(snippets, answers);
+    console.log(scores);
 }
 
-function findAnswerInSearchResults(searchResults) {
-    
+function findAnswerInSearchResults(searchResults, answers) {
+    var answer0Count = getSearchCount(searchResults, answers.answer0);
+    var answer1Count = getSearchCount(searchResults, answers.answer1);
+    var answer2Count = getSearchCount(searchResults, answers.answer2);
+
+    var scores = {
+        answer0Count : answer0Count,
+        answer1Count: answer1Count,
+        answer2Count: answer2Count
+    };
+
+    return scores;
+}
+
+function getSearchCount(searchResults, answer) {
+    //TODO: this matching just matches any letters so it would consider taliban to be an occurenc of "ban". I hope to find a better regex for this, but this is just a hack around for now.
+    var regExp = new RegExp(answer, "gi");
+    var count = (searchResults.match(regExp) || []).length;
+    return count;
 }
 
 function extractSearchSnippets(body) {
     var json = JSON.parse(body);
     var searchResults = json.items;
-    var snippets = [];
+    var snippets = "";
     for (var i = 0; i < searchResults.length; i++) {
         var result = searchResults[i];
-        snippets.push(result.snippet);
+        snippets += result.snippet;
     }
 
     return snippets;
